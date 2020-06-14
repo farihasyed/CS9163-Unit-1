7e34da5a557e43255e2bf38aaced4d09664afb0c
Fariha Syed <fs1688@nyu.edu>
2020-06-14 13:03:33 -0400

    Expand tests

--- test_main.c
+++ test_main.c
@@ -9,6 +9,20 @@ START_TEST(test_dictionary_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
     ck_assert(load_dictionary(TESTDICT, hashtable));
+}
+END_TEST
+
+START_TEST(test_dictionary_edge)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(!load_dictionary("random_file.txt", hashtable));
+}
+END_TEST
+
+START_TEST(test_find_word_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
     const char* correct_word = "hello";
     const char* incorrect_word = "wordl";
     ck_assert(findWord(correct_word, hashtable));
@@ -16,16 +30,163 @@ START_TEST(test_dictionary_normal)
 }
 END_TEST
 
+START_TEST(test_isNumber_normal)
+{
+    char* test_cases[3];
+    test_cases[0] = "1234";
+    test_cases[1] = "63284";
+    test_cases[2] = "number";
+    
+    int i;
+    for (i = 0; i < 3; i++) {
+        if (i < 2) {
+            ck_assert(isNumber(test_cases[i]));
+        } else {
+            ck_assert(!isNumber(test_cases[i]));
+        }
+    }
+}
+END_TEST
+
+START_TEST(test_isNumber_edge)
+{
+    char* test_cases[3];
+    test_cases[0] = "73956293856583027456683658305638462";
+    test_cases[1] = "14mn074numb3r";
+    test_cases[2] = "!@#$123!@#$";
+    
+    int i;
+    for (i = 0; i < 3; i++) {
+        if (i < 1) {
+            ck_assert(isNumber(test_cases[i]));
+        } else {
+            ck_assert(!isNumber(test_cases[i]));
+        }
+    }
+}
+END_TEST
+
+START_TEST(test_removePunctuation_normal)
+{
+    char* test_cases[3];
+    test_cases[0] = "hello world!";
+    test_cases[1] = "\"quotes\"";
+    test_cases[2] = "123%";
+    
+    char* no_punctuation[3];
+    no_punctuation[0] = "hello world";
+    no_punctuation[1] = "quotes";
+    no_punctuation[2] = "123";
+    
+    int i;
+    for (i = 0; i < 3; i++) {
+        ck_assert(strcmp(removePunctuation(test_cases[i]), no_punctuation[i]) == 0);
+    }
+    
+}
+END_TEST
+
+START_TEST(test_removePunctuation_edge)
+{
+    char* test_cases[5];
+    test_cases[0] = "!@#$hello world!@#$\"";
+    test_cases[1] = "@#123";
+    test_cases[2] = "abc123";
+    test_cases[3] = "123abc";
+    test_cases[4] = "#$%^";
+    
+    char* no_punctuation[5];
+    no_punctuation[0] = "hello world";
+    no_punctuation[1] = "123";
+    no_punctuation[2] = "abc123";
+    no_punctuation[3] = "123abc";
+    no_punctuation[4] = "";
+    
+    int i;
+    for (i = 0; i < 5; i++) {
+        ck_assert(strcmp(removePunctuation(test_cases[i]), no_punctuation[i]) == 0);
+    }
+}
+END_TEST
+
+START_TEST(test_toLowercase_normal)
+{
+    char* test_cases[2];
+    test_cases[0] = "Computer";
+    test_cases[1] = "Science";
+    
+    char* lowercase[2];
+    lowercase[0] = "computer";
+    lowercase[1] = "science";
+    
+    int i;
+    for (i = 0; i < 2; i++) {
+        ck_assert(strcmp(removePunctuation(test_cases[i]), lowercase[i]) == 0);
+    }
+}
+END_TEST
+
+START_TEST(test_toLowercase_edge)
+{
+    char* test_cases[3];
+    test_cases[0] = "HeLLo WoRLD";
+    test_cases[1] = "SWITCH";
+    test_cases[2] = "languagE";
+    
+    char* lowercase[3];
+    lowercase[0] = "hello world";
+    lowercase[1] = "switch";
+    lowercase[2] = "language";
+    
+    int i;
+    for (i = 0; i < 3; i++) {
+        ck_assert(strcmp(removePunctuation(test_cases[i]), lowercase[i]) == 0);
+    }
+}
+END_TEST
+
+
 START_TEST(test_check_word_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
     load_dictionary(DICTIONARY, hashtable);
-    const char* correct_word = "Justice";
-    const char* punctuation_word_2 = "pl.ace";
-    const char* quote_word = "\"hello";
-    ck_assert(check_word(correct_word, hashtable));
-    ck_assert(!check_word(punctuation_word_2, hashtable));
-    ck_assert(check_word(quote_word, hashtable));
+    char* test_cases[5];
+    test_cases[0] = "Justice";
+    test_cases[1] = "hello";
+    test_cases[2] = "1234";
+    test_cases[3] = "\"world";
+    test_cases[4] = "pl.ace";
+    
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (i < 4) {
+            ck_assert(check_word(test_cases[i], hashtable));
+        } else {
+            ck_assert(!check_word(test_cases[i], hashtable));
+        }
+    }
+}
+END_TEST
+
+START_TEST(test_check_word_edge)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char* test_cases[5];
+    test_cases[0] = "!@#$hello!@#$\"";
+    test_cases[1] = "abc123";
+    test_cases[2] = "123abc";
+    test_cases[3] = "@#123";
+    test_cases[4] = "#$%^";
+    
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (i < 1) {
+            ck_assert(check_word(test_cases[i], hashtable));
+        } else {
+            ck_assert(!check_word(test_cases[i], hashtable));
+        }
+    }
 }
 END_TEST
 
@@ -59,7 +220,16 @@ check_word_suite(void)
     suite = suite_create("check_word");
     check_word_case = tcase_create("Core");
     tcase_add_test(check_word_case, test_dictionary_normal);
+    tcase_add_test(check_word_case, test_dictionary_edge);
+    tcase_add_test(check_word_case, test_find_word_normal);
+    tcase_add_test(check_word_case, test_isNumber_normal);
+    tcase_add_test(check_word_case, test_isNumber_edge);
+    tcase_add_test(check_word_case, test_removePunctuation_normal);
+    tcase_add_test(check_word_case, test_removePunctuation_edge);
+    tcase_add_test(check_word_case, test_toLowercase_normal);
+    tcase_add_test(check_word_case, test_toLowercase_edge);
     tcase_add_test(check_word_case, test_check_word_normal);
+    tcase_add_test(check_word_case, test_check_word_edge);
     tcase_add_test(check_word_case, test_check_words_normal);
     suite_add_tcase(suite, check_word_case);
     return suite;
